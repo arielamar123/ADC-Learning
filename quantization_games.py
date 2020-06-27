24705d916e29af6fb76828f67e9e105db39cb580
arielamar123 <arielamar123@gmail.com>
2020-06-27 12:01:58 +0300

    tensorflow version of sampling

--- quantization_games.py
+++ quantization_games.py
@@ -2,9 +2,91 @@ import torch
 import numpy as np
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import layers
+
+dense_sampling_L = 20  # Observed discretized time frame
+num_of_adc_p = 4  # Number of scalar quantizers
+num_samples_L_tilde = 4  # Number of samples to produce over time frame
+gaussian_sampling_std = 0.4
+
+
+class SamplingLayer(nn.Module):
+	'''
+	Sampling Pytorch version
+	The input shape to this layer is (batch size, number_of_adc * dense sampling)
+	'''
+
+	def __init__(self):
+		super(SamplingLayer, self).__init__()
+		self.weight = torch.nn.Parameter(
+			data=torch.from_numpy(
+				np.arange(num_samples_L_tilde, dense_sampling_L, num_samples_L_tilde, dtype=np.float64)),
+			requires_grad=True)
+
+	def forward(self, x):
+		out = torch.zeros((len(x), num_samples_L_tilde * num_of_adc_p)).double()
+		t = torch.from_numpy(np.arange(1, dense_sampling_L + 1, dtype=np.double))
+		for v, j in enumerate(range(0, dense_sampling_L * num_of_adc_p, dense_sampling_L)):
+			for k in range(num_samples_L_tilde):
+				out[:, v * num_samples_L_tilde + k] = torch.sum(
+					x[:, j:j + dense_sampling_L] * torch.exp(
+						-(t - self.weight[k]) ** 2 / gaussian_sampling_std ** 2), dim=1)
+		return out
+
+
+class KerasSamplingLayer(tf.keras.layers.Layer):
+	'''
+	Sampling Tensorflow version
+	The input shape to this layer is (batch size, number_of_adc * dense sampling)
+	'''
+
+	def __init__(self):
+		'''
+		initialize sampling layer
+		'''
+		super(KerasSamplingLayer, self).__init__()
+		self.init_weight = np.arange(num_samples_L_tilde, dense_sampling_L, num_samples_L_tilde, dtype=np.float32)
+		self.weight = tf.Variable(initial_value=self.init_weight,
+		                          trainable=True, dtype=tf.float32)
+
+	def call(self, x):
+		# out = tf.Variable(initial_value=np.zeros((len(inp), num_samples_L_tilde * num_of_adc_p), dtype=np.float32))
+		outputs = []
+		t = tf.Variable(initial_value=np.arange(1, dense_sampling_L + 1), dtype=tf.float32)
+		for v, j in enumerate(range(0, dense_sampling_L * num_of_adc_p, dense_sampling_L)):
+			for k in range(num_samples_L_tilde):
+				sampling = tf.math.reduce_sum(
+					x[:, j:j + dense_sampling_L] * tf.exp(
+						-(t - self.weight[k]) ** 2 / gaussian_sampling_std ** 2), axis=1)
+				outputs.append(sampling)
+		return tf.stack(outputs, axis=1)
+
+class HardSamplingLayer(nn.Module):
+	'''
+	Hard sampling Pytorch version
+	TODO : Implement Tensorflow version
+	'''
+	def __init__(self, weight):
+		super(HardSamplingLayer, self).__init__()
+		rounded_weight = torch.round(weight)
+		rounded_weight[rounded_weight < 1] = 1
+		rounded_weight[rounded_weight > dense_sampling_L] = dense_sampling_L
+		self.weight = rounded_weight.long()
+
+	def forward(self, x):
+		out = torch.zeros((len(x), num_samples_L_tilde * num_of_adc_p)).double()
+		for i in range(num_of_adc_p):
+			for j in range(num_samples_L_tilde):
+				out[:, i * num_samples_L_tilde + j] = x[:, i * dense_sampling_L + self.weight[j]]
+		return out
 
 
 class QuantizationLayer(nn.Module):
+	'''
+		Quantization Pytorch version
+		The input shape to this layer is (batch size, number_of_adc * num_samples_L_tilde)
+	'''
 
 	def __init__(self, num_code_words, max_labels, max_samples):
 		'''
@@ -34,7 +116,6 @@ class QuantizationLayer(nn.Module):
 		self.num_code_words = num_code_words
 
 	def forward(self, x):
-		print(x.shape)
 		z = torch.zeros(self.num_code_words - 1, x.shape[0], x.shape[1]).double()
 		for i in range(self.num_code_words - 1):
 			z[i, :, :] = self.a[i] * torch.tanh(self.c[i] * (x - self.b[i]))
@@ -51,11 +132,11 @@ class QuantizationLayer(nn.Module):
 		plt.plot(x_vals, quant)
 
 
-import tensorflow as tf
-from tensorflow.keras import layers
-
-
-class KerasQuantizationLayer(layers.Layer):
+class KerasQuantizationLayer(tf.keras.layers.Layer):
+	'''
+	Quantization Tensorflow version
+	The input shape to this layer is (batch size, number_of_adc * num_samples_L_tilde)
+	'''
 
 	def __init__(self, num_code_words, max_labels, max_samples):
 		'''
@@ -91,6 +172,10 @@ class KerasQuantizationLayer(layers.Layer):
 
 
 class HardQuantizationLayer(nn.Module):
+	'''
+	Hard quantization Pytorch version
+	TODO : Implement Tensorflow version
+	'''
 
 	def __init__(self, a, b, c, num_code_words):
 		super(HardQuantizationLayer, self).__init__()
@@ -119,12 +204,12 @@ class HardQuantizationLayer(nn.Module):
 
 
 if __name__ == '__main__':
-	x = tf.ones((4, 4), dtype=tf.float64)
-	print('**********************')
-	tf.keras.backend.print_tensor(
-		x, message='hello'
-	)
-	print('**********************')
-
-	# quant_layer = KerasQuantizationLayer(8, 15, 15)
-	# y = quant_layer(x)
+	# inp = tf.ones((1, num_of_adc_p * dense_sampling_L), dtype=tf.float32)
+	# print(inp)
+	inp2 = tf.range(1, 81, dtype=tf.float32)
+	rep = tf.constant([4])
+	inp2 = tf.reshape(tf.tile(inp2, rep), [4, 80])
+	print(inp2.shape)
+	k_smp = KerasSamplingLayer()
+	out = k_smp(inp2)
+	print(out)
